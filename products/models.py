26d1899e438b0f9ce663459c85be585d6af6633a
Mehyar Farzat <mehyar.farzat@gmail.com>
2024-01-07 13:20:27 +0100

    create save function to activate a slug

--- products/models.py
+++ products/models.py
@@ -26,6 +26,10 @@ class Product(models.Model):
         return self.name
     
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Product,self).save(*args, **kwargs)
+
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_image')
     image = models.ImageField(upload_to='product_images')
