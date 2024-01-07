7027e7ad7f0bc61be436864b0b9217d91815ec34
Mehyar Farzat <mehyar.farzat@gmail.com>
2024-01-07 13:01:03 +0100

    import summernote and register SummernoteModelAdmin in admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Product, ProductImage,Review
 
-# Register your models here.
 
-admin.site.register(Product)
+class ProductAdmin(SummernoteModelAdmin):
+    summernote_fields = '__all__'
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductImage)
 admin.site.register(Review)
