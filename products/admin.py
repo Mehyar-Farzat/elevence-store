1b74da0f7dc3801fa2be067aa7f31d37768c1e45
Mehyar Farzat <mehyar.farzat@gmail.com>
2024-01-07 13:11:08 +0100

    add django admin inline to merge ProductImage with Product

--- products/admin.py
+++ products/admin.py
@@ -3,8 +3,13 @@ from django_summernote.admin import SummernoteModelAdmin
 from .models import Product, ProductImage,Review
 
 
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+
+
 class ProductAdmin(SummernoteModelAdmin):
     summernote_fields = '__all__'
+    inlines = [ProductImageInline]
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductImage)
