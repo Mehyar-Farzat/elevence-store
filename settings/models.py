7c7ffe8424bb50bc712d13efab229b1959d5cf56
Mehyar Farzat <mehyar.farzat@gmail.com>
2024-01-08 20:08:49 +0100

    create context_processor file and add it in settings of project

--- settings/models.py
+++ settings/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Company(models.Model):
     name = models.CharField(max_length=50)
+    logo = models.ImageField(upload_to='logo/')
     description = models.TextField(max_length=1000)
     fb_link = models.URLField(null=True,blank=True)
     x_link = models.URLField(null=True,blank=True)
