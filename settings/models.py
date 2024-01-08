700c555172283e3e66f0484ac6d7783ebf2fdf57
Mehyar Farzat <mehyar.farzat@gmail.com>
2024-01-08 19:45:37 +0100

    create company model

--- settings/models.py
+++ settings/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Company(models.Model):
+    description = models.TextField(max_length=1000)
+    fb_link = models.URLField(null=True,blank=True)
+    x_link = models.URLField(null=True,blank=True)
+    instagram_link = models.URLField(null=True,blank=True)
+    lenkedin_link = models.URLField(null=True,blank=True)
+
+
+    def __str__(self):
+        return self.name
+    
