324fcccc7c638cf14b6496288d862a1d95b8ad80
Mehyar Farzat <mehyar.farzat@gmail.com>
2024-01-08 19:51:06 +0100

    add name field to company model

--- settings/models.py
+++ settings/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Company(models.Model):
+    name = models.CharField(max_length=50)
     description = models.TextField(max_length=1000)
     fb_link = models.URLField(null=True,blank=True)
     x_link = models.URLField(null=True,blank=True)
